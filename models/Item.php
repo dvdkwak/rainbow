4319007e9480431ebe3814090beed60c646c93e5
dvdkwak <dvdkwak@gmail.com>
2021-01-03 16:00:03 +0100

    setup Item model

---
+++ models/Item.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Item is the main object within this project
+ * Properties:
+ * - ID
+ * - Title
+ * - Uri
+ * - Short
+ * - Content
+ * - Author
+ * - Tags
+ * - Keywords
+ * - Active
+ * - Deleted
+ * - Date_Created
+ * - Date_Live
+ * - Date_Deleted
+ */
+
+class Item {
+
+    public $id;
+    public $title;
+    public $uri;
+    public $short;
+    public $content;
+    public $author;
+    public $tags;
+    public $keywords;
+    public $active;
+    public $deleted;
+    public $dateCreated;
+    public $dateLive;
+    public $dateDeleted;
+
+}
