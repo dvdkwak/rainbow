ed60609f1600f535f1d21e80d3a5d6d25664a5fb
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2021-04-04 18:36:28 +0200

    added a few menu items

--- views/components/menu.php
+++ views/components/menu.php
@@ -5,6 +5,7 @@
     <li><a href="<?= DIRECTORY ?>admin/item">Items</a></li>
     <li><a href="<?= DIRECTORY ?>admin/goodies">Goodies</a></li>
     <li><a href="<?= DIRECTORY ?>admin/tags">Tags</a></li>
+    <li><a href="<?= DIRECTORY ?>admin/author">Authors</a></li>
     <li><a href="<?= DIRECTORY ?>admin/settings">Settings</a></li>
     <li><a href="<?= DIRECTORY ?>admin/coats">Coats</a></li>
   </ul>
