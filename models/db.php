67ff65c0579b8193ed370c4349842126dc70cfdb
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2021-04-04 18:30:41 +0200

    Added a connection check with feedback on DEBUGMODE

--- models/db.php
+++ models/db.php
@@ -8,6 +8,9 @@ class db extends Mysqli {
 
   public function __construct() {
     @parent::__construct(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    if($this->connect_error && DEBUGMODE) {
+      echo "<b>Error: </b>" . $this->connect_error;
+    }
   }
 
 }
