2362ed65269cec77eb85b6144806f9ade2ac4e42
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2021-04-04 18:38:01 +0200

    added a route item/[id] this will build a page of a specific item

---
+++ views/item.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?= $item->short ?>">
+    <meta name="keywords" content="<?= $item->keywords ?>">
+    <meta name="author" content="<?= $item->author ?>">
+    <title><?= $item->title ?></title>
+</head>
+<body>
+    <?= $item->content ?>
+</body>
+</html>
