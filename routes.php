5c4ee54ac950b13cd0135b09849c10b3652a710b
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2021-04-04 18:29:20 +0200

    added a few routes ...

--- routes.php
+++ routes.php
@@ -11,6 +11,10 @@ $route->add('admin', 'admin/home.php', 'admin/home.php');
 
 // admin routes
 $route->add('admin/page', 'admin/page.php', 'admin/page.php');
-$route->add('admin/item', 'admin/item.php');
+$route->add('admin/item', 'admin/item.php', 'admin/item.php');
+$route->add('admin/author', 'admin/author.php', 'admin/author.php');
+
+// Rainbow routes
+$route->add('item/{itemId}', 'item.php', 'item.php');
 
 // _________________________________________________________________________
