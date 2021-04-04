45a2500843ac4d28039f742e757be854d92cd468
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2021-04-04 18:31:47 +0200

    added a controller with a method to get all pages from the database

--- controllers/admin/page.php
+++ controllers/admin/page.php
@@ -1,3 +1,12 @@
 <?php
 
 $pageTitle = "Admin | Page";
+
+// retrieving the top 20 pages
+$pageController = new PageController();
+$pages = $pageController->getAll(20);
+
+// if no items retrieved, pass an array to display an error
+if(!$pages) {
+    $pages = [];
+}
--- views/admin/page.php
+++ views/admin/page.php
@@ -1,7 +1,28 @@
 <?php include_once view('components/header.php', True); ?>
 
-<h1>Page</h1>
+<h1>Items</h1>
 
 <?php view('components/menu.php'); ?>
+
+<table>
+    <thead>
+        <th>#</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Active</th>
+        <th>Date Created</th>
+    </thead>
+    <tbody>
+        <?php foreach($pages as $page) : ?>
+            <tr>
+                <td><?= $page->id ?></td>
+                <td><?= $page->title ?></td>
+                <td><?= $page->author ?></td>
+                <td><?= $page->active ?></td>
+                <td><?= $page->dateCreated ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
 
 <?php view('components/footer.php'); ?>
